912733fcd4f7fb418f90deec483d68e2d3ff6f6f
chunlong.ni <anycase@tongdun.cn>
2020-04-08 16:34:18 +0800

    整理工程

--- extension/index.ts
+++ extension/index.ts
@@ -2,55 +2,54 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
 type Snippet = {
-  body: Array<string> | string
-  description: string
-  prefix: Array<string> | string
+    body: Array<string> | string
+    description: string
+    prefix: Array<string> | string
 }
 const snippets = require('../snippets/snippets.json')
-const dvaSnippets = require('../snippets/dva.json');
 const tntSnippets = require('../snippets/tnt.json');
-const allSnippets = Object.assign({},tntSnippets,dvaSnippets,snippets)
+const allSnippets = Object.assign({}, tntSnippets, snippets)
 
 const convertSnippetArrayToString = (snippetArray: Array<string>): string => snippetArray.join('\n')
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
+    // Use the console to output diagnostic information (console.log) and errors (console.error)
+    // This line of code will only be executed once when your extension is activated
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.snippetSearch', async () => {
-	const items = Object.entries(allSnippets as Array<Snippet>).map(
-      ([shortDescription, { prefix, body, description }], index) => {
-        const value = typeof prefix === 'string' ? prefix : prefix[0]
-        return {
-          id: index,
-          description: description || shortDescription,
-          label: value,
-          value,
-          body,
+    // The command has been defined in the package.json file
+    // Now provide the implementation of the command with registerCommand
+    // The commandId parameter must match the command field in package.json
+    let disposable = vscode.commands.registerCommand('extension.snippetSearch', async () => {
+        const items = Object.entries(allSnippets as Array<Snippet>).map(
+            ([shortDescription, { prefix, body, description }], index) => {
+                const value = typeof prefix === 'string' ? prefix : prefix[0]
+                return {
+                    id: index,
+                    description: description || shortDescription,
+                    label: value,
+                    value,
+                    body,
+                }
+            },
+        )
+
+        const options = {
+            matchOnDescription: true,
+            matchOnDetail: true,
+            placeHolder: 'Search snippet',
         }
-      },
-    )
 
-    const options = {
-      matchOnDescription: true,
-      matchOnDetail: true,
-      placeHolder: 'Search snippet',
-    }
+        const snippet = (await vscode.window.showQuickPick(items, options)) || { body: '' }
+        const activeTextEditor = vscode.window.activeTextEditor
+        const body =
+            typeof snippet.body === 'string' ? snippet.body : convertSnippetArrayToString(snippet.body)
+        activeTextEditor && activeTextEditor.insertSnippet(new vscode.SnippetString(body))
+    })
 
-    const snippet = (await vscode.window.showQuickPick(items, options)) || { body: '' }
-    const activeTextEditor = vscode.window.activeTextEditor
-    const body =
-      typeof snippet.body === 'string' ? snippet.body : convertSnippetArrayToString(snippet.body)
-    activeTextEditor && activeTextEditor.insertSnippet(new vscode.SnippetString(body))
-  })
-
-  context.subscriptions.push(disposable)
+    context.subscriptions.push(disposable)
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
